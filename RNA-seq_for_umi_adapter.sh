ed18b12bd7b4b2b833a898bce730b4dad400449b
yihsintsai <100409332+yihsintsai@users.noreply.github.com>
2023-03-29 09:47:02 +0800

    Update RNA-seq_for_umi_adapter.sh

--- RNA-seq_for_umi_adapter.sh
+++ RNA-seq_for_umi_adapter.sh
@@ -207,7 +207,7 @@ ${UMITOOLCMD}  dedup \
 ${SAMTOOLSCMD} view -@ ${CPU} \
 -F 524 -f 2 \
 ${OUTPREFIXR1}_UMI_dedup.bam \
-| ${SAMTOOLSCMD} sort -@ ${CPU} -u -o ${OUTPREFIXR1}_UMI_filter.bam
+| ${SAMTOOLSCMD} sort -@ ${CPU} -n -o ${OUTPREFIXR1}_UMI_filter.bam
 echo $(date) samtools de-duplication COMPLETED
 
 
